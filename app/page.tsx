9bbbafa8fb9b3422eb9c526e59dbc31d05826ea3
Anna Kovalik <annhuddy13@gmail.com>
2023-08-13 16:54:19 +0300

    Chore: change file structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,10 @@
-import Image from 'next/image';
-import styles from './page.module.css';
-
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
+    <main>
+      <div>
         <h2>Next.js Typescript project template</h2>
         <h3>Tools:</h3>
-        <ul className={styles.list}>
+        <ul>
           <li>Next.js</li>
           <li>Typescript</li>
           <li>Eslint</li>
